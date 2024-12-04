45e6042e37b47b1b3d5a6306bc3fa96bd128e723
Keyvan <keyvan42@pm.me>
2024-12-04 22:32:52 +0100

    Make access key and secret key optional

--- cluster/variables.tf
+++ cluster/variables.tf
@@ -7,12 +7,14 @@ variable "AWS_REGION" {
 variable "AWS_ACCESS_KEY_ID" {
   type        = string
   description = "AWS access key associated with an IAM account"
+  default     = ""
   sensitive   = true
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
   type        = string
   description = "AWS secret key associated with the access key"
+  default     = ""
   sensitive   = true
 }
 
