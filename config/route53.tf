26c412a43c470e427fe417556478c91edca43ef4
Xi Bai <82581439+baixiac@users.noreply.github.com>
2025-04-11 17:39:45 +0100

    import the primary hosted zone via data sources and add DMARC compliance (#49)

--- config/route53.tf
+++ config/route53.tf
@@ -2,25 +2,14 @@ locals {
   domain_name = length(var.domain_name) == 0 ? null : keys(var.domain_name)[0]
 }
 
-import {
-  for_each = var.domain_name
-  to       = aws_route53_zone.primary[0]
-  id       = each.value
-}
-
-resource "aws_route53_zone" "primary" {
-  count = var.enable_route53 && length(var.domain_name) == 1 ? 1 : 0
-  name  = local.domain_name
-  tags  = merge(tomap({ "Name" : "${var.eks_cluster_name}-primary-zone" }), var.common_tags)
-
-  #checkov:skip=CKV2_AWS_39: This will result in extra charge and should be only enabled for troubleshooting and stringent auditing
-  #checkov:skip=CKV2_AWS_38: DNSSEC signing needs to be optional
+data "aws_route53_zone" "primary" {
+  name = local.domain_name
 }
 
 resource "aws_route53_record" "main" {
   count = var.enable_route53 && length(var.domain_name) == 1 && var.enable_eip ? 1 : 0
 
-  zone_id = aws_route53_zone.primary[0].zone_id
+  zone_id = data.aws_route53_zone.primary.zone_id
   name    = "${var.environment}.${local.domain_name}"
   type    = "CNAME"
   ttl     = 300
@@ -30,7 +19,7 @@ resource "aws_route53_record" "main" {
 resource "aws_route53_record" "this" {
   for_each = toset([for prefix in local.cname_prefixes : prefix if var.enable_route53 && length(var.domain_name) == 1])
 
-  zone_id = aws_route53_zone.primary[0].zone_id
+  zone_id = data.aws_route53_zone.primary.zone_id
   name    = "${each.value}.${var.environment}.${local.domain_name}"
   type    = "CNAME"
   ttl     = 300
@@ -44,7 +33,7 @@ module "external_dns_irsa" {
 
   role_name                     = "${var.eks_cluster_name}-external-dns-irsa"
   attach_external_dns_policy    = true
-  external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/${aws_route53_zone.primary[0].zone_id}"]
+  external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/${data.aws_route53_zone.primary.zone_id}"]
 
   oidc_providers = {
     ex = {
@@ -63,7 +52,7 @@ module "cert_manager_irsa" {
 
   role_name                     = "${var.eks_cluster_name}-cert-manager-irsa"
   attach_cert_manager_policy    = true
-  cert_manager_hosted_zone_arns = ["arn:aws:route53:::hostedzone/${aws_route53_zone.primary[0].zone_id}"]
+  cert_manager_hosted_zone_arns = ["arn:aws:route53:::hostedzone/${data.aws_route53_zone.primary.zone_id}"]
 
   oidc_providers = {
     main = {
@@ -76,5 +65,5 @@ module "cert_manager_irsa" {
 }
 
 output "radar_base_route53_hosted_zone_id" {
-  value = var.enable_route53 && length(var.domain_name) == 1 ? aws_route53_zone.primary[0].zone_id : null
+  value = var.enable_route53 && length(var.domain_name) == 1 ? data.aws_route53_zone.primary.zone_id : null
 }
